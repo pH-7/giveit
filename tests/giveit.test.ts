5dfe49ce7391edbea8f2d1f71a474bbf6f5de5c2
Pierre-Henry Soria <pierre@ph7.me>
2024-05-18 11:55:41 +1000

    Reformat/clean unit tests

--- tests/giveit.test.ts
+++ tests/giveit.test.ts
@@ -2,7 +2,7 @@ import test from "ava";
 import chalk from "chalk";
 import { giveit } from "../src/index";
 
-test("giveit should handle complex nested objects", (t) => {
+test("must handle complex nested objects", (t) => {
   const input = {
     name: "John",
     age: 30,
@@ -58,7 +58,7 @@ test("giveit should handle complex nested objects", (t) => {
   t.is(result, expected);
 });
 
-test("giveit should handle empty objects and arrays", (t) => {
+test("must handle empty objects and arrays", (t) => {
   const input = {
     emptyObject: {},
     emptyArray: [],
@@ -73,43 +73,47 @@ test("giveit should handle empty objects and arrays", (t) => {
   t.is(result, expected);
 });
 
-test("giveit should colorize string values", (t) => {
+test("must colorize string values", (t) => {
   const input = { name: "John" };
   const expected = `{
   ${chalk.green('"name"')}: ${chalk.green('"John"')}
 }`;
+
   const result = giveit(input);
   t.is(result, expected);
 });
 
-test("giveit should colorize number values", (t) => {
+test("must colorize number values", (t) => {
   const input = { age: 25 };
   const expected = `{
   ${chalk.green('"age"')}: ${chalk.yellow("25")}
 }`;
+
   const result = giveit(input);
   t.is(result, expected);
 });
 
-test("giveit should colorize boolean values", (t) => {
+test("must colorize boolean values", (t) => {
   const input = { isStudent: true };
   const expected = `{
   ${chalk.green('"isStudent"')}: ${chalk.blue("true")}
 }`;
+
   const result = giveit(input);
   t.is(result, expected);
 });
 
-test("giveit should colorize null values", (t) => {
+test("must colorize null values", (t) => {
   const input = { address: null };
   const expected = `{
   ${chalk.green('"address"')}: ${chalk.red("null")}
 }`;
+
   const result = giveit(input);
   t.is(result, expected);
 });
 
-test("giveit should handle nested objects", (t) => {
+test("must handle nested objects", (t) => {
   const input = {
     person: {
       name: "John",
@@ -126,16 +130,18 @@ test("giveit should handle nested objects", (t) => {
     ${chalk.green('"address"')}: ${chalk.red("null")}
   }
 }`;
+
   const result = giveit(input);
   t.is(result, expected);
 });
 
-test("giveit should use the provided indentation", (t) => {
+test("must use the provided indentation", (t) => {
   const input = { name: "John", age: 25 };
   const expected = `{
     ${chalk.green('"name"')}: ${chalk.green('"John"')},
     ${chalk.green('"age"')}: ${chalk.yellow("25")}
 }`;
+
   const result = giveit(input, 4);
   t.is(result, expected);
 });
