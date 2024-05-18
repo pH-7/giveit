e1645bdb706cf8d8753cfe954e6d6f9e49d70dac
♚ PH⑦ de Soria™♛ <pierre@ph7.me>
2024-05-18 11:17:52 +1000

    Cleanup: Use guard clauses approach
    https://github.com/pH-7/GoodJsCode?tab=readme-ov-file#guard-clauses-approach

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import chalk from "chalk";
 
-export function giveit(obj: any, indent = 2): string {
+export function giveit(variable: any, indent = 2): string {
   const colorize = (value: any): string => {
     if (typeof value === "string") {
       return chalk.green(`"${value}"`);
-    } else if (typeof value === "number") {
+    }
+    if (typeof value === "number") {
       return chalk.yellow(value.toString());
-    } else if (typeof value === "boolean") {
+    }
+    if (typeof value === "boolean") {
       return chalk.blue(value.toString());
-    } else if (value === null) {
+    }
+    if (value === null) {
       return chalk.red("null");
     }
     return value;
   };
 
-  const jsonString = JSON.stringify(obj, null, indent);
+  const jsonString = JSON.stringify(variable, null, indent);
 
   const colorizedString = jsonString.replace(
     /(\".*?\"|true|false|null|\d+)/g,
